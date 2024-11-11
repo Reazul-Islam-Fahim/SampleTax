5878dc4b276a65c341cab4b61769fb92faef874d
Reazul Islam Fahim <jkabbo@yahoo.com>
2024-11-11 12:58:23 +0600

    update

--- schemas.py
+++ schemas.py
@@ -30,12 +30,64 @@ class TaxPayers(BaseModel):
     bin_no : str
     name_tin_partners : str |None = None 
     
+    class Config:
+        orm_mode = True
+    
 
 class TaxPayerCreate(TaxPayers):
     pass
 
-class TaxPayer(TaxPayers):
-    pass
+
+
+
+class SalaryIncomeRecord(BaseModel):
+    id: int
+    basic_salary: int
+    house_rent_allowance: int
+    medical_allowance: int
+    festival_bonus: int
 
     class Config:
         orm_mode = True
+
+
+class PrivateSalaryIncomeRecord(SalaryIncomeRecord):
+    rent_free_accommodation: int = 0
+    accommodation_at_concessional_rate: int = 0
+    rent_paid_by_taxpayer: int = 0
+    vehicle_facility_months: int = 0
+    is_higher_cc: str = 'N'  # 'Y' or 'N'
+    other_non_cash_benefits: int = 0  # Store as JSON (stringified dict)
+    num_autistic_children: int = 0
+    arrear_salary: int = 0
+    education_allowance: int = 0
+    entertainment_allowance: int = 0
+    employer_contribution_RPF: int = 0
+    gratuity: int = 0
+    interest_accrued_RPF: int = 0
+    leave_allowance: int = 0
+    other_bonus: int = 0
+    overtime: int = 0
+    pension: int = 0
+    tada: int = 0
+    income_from_employee_share_scheme: int = 0
+    others: int = 0
+    allowances: int = 0
+    perquisites: int = 0
+    etin: int
+    
+    
+    
+class GovSalaryIncomeRecord(BaseModel):
+    arrear_pay: int = 0
+    special_allowance: int = 0
+    conveyance_allowance: int = 0
+    support_staff_allowance: int = 0
+    leave_allowance: int = 0
+    reward: int = 0
+    overtime: int = 0
+    bangla_noboborsho: int = 0
+    interest_accrued_from_PF: int = 0
+    lump_grant: int = 0
+    gratuity: int = 0
+    others: int = 0
--- tax_payer.py
+++ tax_payer.py
@@ -12,14 +12,14 @@ app = FastAPI()
 def create_tax_payer(taxpayer: schemas.TaxPayerCreate = Query(...), db: Session = Depends(get_db)): #Body(...) can be use instead of Query(...) for json format
     return crud.create_tax_payer(db=db, tax_payer=taxpayer)
 
-@app.get("/tax_payer/{tax_payer_id}", response_model=schemas.TaxPayer)
+@app.get("/tax_payer/{tax_payer_id}", response_model=schemas.TaxPayers)
 def read_tax_payer(tax_payer_etin: int, db: Session = Depends(get_db)):
     db_item = crud.get_tax_payer(db, tax_payer_etin = tax_payer_etin)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
 
-@app.get("/tax_payers/", response_model=list[schemas.TaxPayer])
+@app.get("/tax_payers/", response_model=list[schemas.TaxPayers])
 def read_tax_payers(skip: int = Query(...), limit: int = Query(...), db: Session = Depends(get_db)):
     items = crud.get_tax_payers(db, skip=skip, limit=limit)
     return items
