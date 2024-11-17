d3d8c5af775194e568c148ebc81a0c2e944baf98
Reazul Islam Fahim <jkabbo@yahoo.com>
2024-11-17 17:44:38 +0600

    new

---
+++ investment_calculator.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Depends, HTTPException, Body, Query
+from sqlalchemy.orm import Session
+import crud, models, schemas
+from db import SessionLocal, engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/investment_record/", response_model=schemas.Investment_Record)
+def create_investment_record(investment_record: schemas.Investment_Record = Body(...), db: Session = Depends(get_db)): #Body(...) can be use instead of Query(...) for json format
+    return crud.create_investment_record(db=db, investment_record=investment_record)
+
+@app.get("/investment_record/{investment_record_id}", response_model=schemas.Investment_Record)
+def read_investment_record(etin: str, db: Session = Depends(get_db)):
+    db_item = crud.get_investment_record(db, etin= etin)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@app.get("/investment_records/", response_model=list[schemas.Investment_Record])
+def read_investment_records(skip: int = Query(...), limit: int = Query(...), db: Session = Depends(get_db)):
+    items = crud.get_investment_records(db, skip=skip, limit=limit)
+    return items
+
+@app.get("/calculate_allowable_investment/{investment_record_id}", response_model=schemas.Investment_Record)
+def calc_allowable_investment(etin : str, db : Session = Depends(get_db)):
+    pass
--- schemas.py
+++ schemas.py
@@ -235,12 +235,86 @@ class SalaryIncome_Summary(BaseModel):
         
 class Investment_Record(BaseModel):
     etin: str
-    gov_securities : int = 0
-    eft : int = 0
-    life_insurance_policy_value : int = 0
-    life_insurance_given_premium : int = 0
-    other : int = 0  
-      
+    gov_securities: int = 0
+    gov_securities_remarks: Optional[str] = None
+    eft: int = 0
+    eft_remarks: Optional[str] = None
+    life_insurance_policy_value: int = 0
+    life_insurance_policy_value_remarks: Optional[str] = None
+    life_insurance_given_premium: int = 0
+    life_insurance_given_premium_remarks: Optional[str] = None
+    premium_or_contractual_deferred_annuity_of_life_insurance_policy_paid_in_Bangladesh: int = 0
+    premium_or_contractual_deferred_annuity_of_life_insurance_policy_paid_in_Bangladesh_remarks: Optional[str] = None
+    contribution_paid_to_deposit_pension_or_monthly_savings_scheme: int = 0
+    contribution_paid_to_deposit_pension_or_monthly_savings_scheme_remarks: Optional[str] = None
+    investment_in_any_securities_listed_with_an_authorized_stock_exchange: int = 0
+    investment_in_any_securities_listed_with_an_authorized_stock_exchange_remarks: Optional[str] = None
+    provisions_of_pf_Act_1925_apply_to_the_contribution_of_the_taxpayer_to_any_such_fund: int = 0
+    provisions_of_pf_Act_1925_apply_to_the_contribution_of_the_taxpayer_to_any_such_fund_remarks: Optional[str] = None
+    contributions_made_by_the_taxpayer_and_his_employer_to_an_approved_provident_fund: int = 0
+    contributions_made_by_the_taxpayer_and_his_employer_to_an_approved_provident_fund_remarks: Optional[str] = None
+    contributions_paid_to_approved_superannuation_funds: int = 0
+    contributions_paid_to_approved_superannuation_funds_remarks: Optional[str] = None
+    contribution_paid_to_welfare_fund_group_insurance_fund: int = 0
+    contribution_paid_to_welfare_fund_group_insurance_fund_remarks: Optional[str] = None
+    contribution_paid_to_zakat_fund: int = 0
+    contribution_paid_to_zakat_fund_remarks: Optional[str] = None
+    donation_to_any_national_level_institution_dedicated_to_the_preservation_of_the_memory_of_the_liberation_war: int = 0
+    donation_to_any_national_level_institution_dedicated_to_the_preservation_of_the_memory_of_the_liberation_war_remarks: Optional[str] = None
+    donations_to_national_institutions_to_preserve_the_memory_of_the_father_of_the_nation: int = 0
+    donations_to_national_institutions_to_preserve_the_memory_of_the_father_of_the_nation_remarks: Optional[str] = None
+    donation_to_organizations_established_for_the_welfare_of_the_disabled: int = 0
+    donation_to_organizations_established_for_the_welfare_of_the_disabled_remarks: Optional[str] = None
+    donations_made_to_the_liberation_war_museum: int = 0
+    donations_made_to_the_liberation_war_museum_remarks: Optional[str] = None
+    donation_to_ahsania_cancer_hospital: int = 0
+    donation_to_ahsania_cancer_hospital_remarks: Optional[str] = None
+    donations_made_to_icddrb: int = 0
+    donations_made_to_icddrb_remarks: Optional[str] = None
+    donation_given_at_crp_savar: int = 0
+    donation_given_at_crp_savar_remarks: Optional[str] = None
+    donations_to_charitable_or_educational_institutions_approved_by_the_government: int = 0
+    donations_to_charitable_or_educational_institutions_approved_by_the_government_remarks: Optional[str] = None
+    donation_to_asiatic_society_bangladesh: int = 0
+    donation_to_asiatic_society_bangladesh_remarks: Optional[str] = None
+    donation_to_dhaka_ahsania_mission_cancer_hospital: int = 0
+    donation_to_dhaka_ahsania_mission_cancer_hospital_remarks: Optional[str] = None
+    contribution_paid_to_super_annuity_fund: int = 0
+    contribution_paid_to_super_annuity_fund_remarks: Optional[str] = None
+    other: int = 0
+    other_remarks: Optional[str] = None
+    total : int =0
+    allowable_investment : int = 0
+
+    @property
+    def total(self):
+        return (
+            self.gov_securities +
+            self.eft +
+            self.life_insurance_policy_value +
+            self.life_insurance_given_premium +
+            self.premium_or_contractual_deferred_annuity_of_life_insurance_policy_paid_in_Bangladesh +
+            self.contribution_paid_to_deposit_pension_or_monthly_savings_scheme +
+            self.investment_in_any_securities_listed_with_an_authorized_stock_exchange +
+            self.provisions_of_pf_Act_1925_apply_to_the_contribution_of_the_taxpayer_to_any_such_fund +
+            self.contributions_made_by_the_taxpayer_and_his_employer_to_an_approved_provident_fund +
+            self.contributions_paid_to_approved_superannuation_funds +
+            self.contribution_paid_to_welfare_fund_group_insurance_fund +
+            self.contribution_paid_to_zakat_fund +
+            self.donation_to_any_national_level_institution_dedicated_to_the_preservation_of_the_memory_of_the_liberation_war +
+            self.donations_to_national_institutions_to_preserve_the_memory_of_the_father_of_the_nation +
+            self.donation_to_organizations_established_for_the_welfare_of_the_disabled +
+            self.donations_made_to_the_liberation_war_museum +
+            self.donation_to_ahsania_cancer_hospital +
+            self.donations_made_to_icddrb +
+            self.donation_given_at_crp_savar +
+            self.donations_to_charitable_or_educational_institutions_approved_by_the_government +
+            self.donation_to_asiatic_society_bangladesh +
+            self.donation_to_dhaka_ahsania_mission_cancer_hospital +
+            self.contribution_paid_to_super_annuity_fund +
+            self.other
+        )
+
     class Config:
         orm_mode = True
         
