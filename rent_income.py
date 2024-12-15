d53fd99805fe4679375e7c8639e94570f74955f9
Reazul Islam Fahim <jkabbo@yahoo.com>
2024-12-15 17:11:57 +0600

    update

--- rent_income.py
+++ rent_income.py
@@ -72,76 +72,89 @@ def create_rent_details_income(
         area_rate = 0.25
     elif(rent_income_master.area_type.lower() == "commercial"):
         area_rate = 0.3
+
         
-    
     others_count_flat = 0    
     total_count_flat = 0
     
-        
+    rent_income_master.total_adjusted_advance = 0
     rent_income_details.total_vacancy_month = 0
     total_rent_month = 0
     rent_income_master.total_adjusted_advance = 0
         
-    if(rent_income_details.live_ownself.lower() == "yes"):
-        total_rent_month = 12
-        total_count_flat+=1
-    else:
-        if(rent_income_details.january.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.february.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.march.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.april.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.may.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.june.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.july.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.august.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.september.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.october.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.november.lower() == "yes"):
-            total_rent_month+=1
-        if(rent_income_details.december.lower() == "yes"):
-            total_rent_month+=1  
+    while(True):
+            
+        if(rent_income_details.live_ownself.lower() == "yes"):
+            total_rent_month = 12
+            total_count_flat+=1
+        else:
+            if(rent_income_details.january.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.february.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.march.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.april.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.may.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.june.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.july.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.august.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.september.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.october.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.november.lower() == "yes"):
+                total_rent_month+=1
+            if(rent_income_details.december.lower() == "yes"):
+                total_rent_month+=1  
+            
+            others_count_flat+=1
+            total_count_flat+=1
+            
         
-        others_count_flat+=1
-        total_count_flat+=1
+        rent_income_details.total_vacancy_month = 12 - total_rent_month
+        
+        rent_income_master.total_income = (rent_income_details.monthly_rent * total_rent_month) + (rent_income_details.monthly_service_charge * total_rent_month) + rent_income_master.other_taken_rent
+        
+        rent_income_master.yearly_value = rent_income_details.monthly_rent * 12
+        
+        rent_income_details.adjusted_advance = rent_income_details.advance - rent_income_details.adjusted_rent
+        
+        rent_income_master.total_adjusted_advance += rent_income_details.adjusted_advance 
+        
+        rent_income_master.receipt_of_repairs_allowable = rent_income_master.total_income * area_rate
         
         
-    rent_income_details.total_vacancy_month = 12 - total_rent_month
-    
-    rent_income_master.total_income = (rent_income_details.monthly_rent * total_rent_month) + (rent_income_details.monthly_service_charge * total_rent_month) + rent_income_master.other_taken_rent
-    
-    rent_income_master.receipt_of_repairs_allowable = rent_income_master.total_income * area_rate
-    
-    if(rent_income_master.receipt_of_repairs_actual > rent_income_master.receipt_of_repairs_allowable):
-        # rent.special_income = rent.receipt_of_repairs_allowable
-        rent_income_master.special_income = 0
-        rent_income_master.net_income = rent_income_master.total_income - rent_income_master.receipt_of_repairs_allowable
-    else:
-        rent_income_master.special_income = rent_income_master.receipt_of_repairs_allowable - rent_income_master.receipt_of_repairs_actual
-        rent_income_master.net_income = rent_income_master.total_income - rent_income_master.receipt_of_repairs_actual
         
-    
-    rent_income_master.total_expense = (rent_income_master.insurance_premium_paid_allowable + rent_income_master.interest_on_repaid_loans_allowable + 
-                                        rent_income_master.land_revenue_allowable + rent_income_master.municipal_or_local_tax_allowable + rent_income_master.receipt_of_repairs_allowable)
-    
-    rent_income_master.net_income = rent_income_master.net_income - (rent_income_master.insurance_premium_paid_allowable + rent_income_master.interest_on_repaid_loans_allowable + 
-                                                                       rent_income_master.land_revenue_allowable + rent_income_master.municipal_or_local_tax_allowable)
-    
-    
-    
-    
-    
-     
+        
+        if(rent_income_master.receipt_of_repairs_actual > rent_income_master.receipt_of_repairs_allowable):
+            # rent.special_income = rent.receipt_of_repairs_allowable
+            rent_income_master.special_income = 0
+            rent_income_master.net_income = rent_income_master.total_income - rent_income_master.receipt_of_repairs_allowable
+        else:
+            rent_income_master.special_income = rent_income_master.receipt_of_repairs_allowable - rent_income_master.receipt_of_repairs_actual
+            rent_income_master.net_income = rent_income_master.total_income - rent_income_master.receipt_of_repairs_actual
+            
+        
+            
+        
+        rent_income_master.total_expense = (rent_income_master.insurance_premium_paid_allowable + rent_income_master.interest_on_repaid_loans_allowable + 
+                                            rent_income_master.land_revenue_allowable + rent_income_master.municipal_or_local_tax_allowable + rent_income_master.receipt_of_repairs_allowable)
+        
+        rent_income_master.net_income = rent_income_master.net_income - (rent_income_master.insurance_premium_paid_allowable + rent_income_master.interest_on_repaid_loans_allowable + 
+                                                                        rent_income_master.land_revenue_allowable + rent_income_master.municipal_or_local_tax_allowable)
+        
+        
+        
+        
+        
+        
     
     print(etin)
     
-    return rent_income_details
+    return rent_income_master
